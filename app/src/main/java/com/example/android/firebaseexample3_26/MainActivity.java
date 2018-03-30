2ba308437f5fd7f39211fd1e143fd8d35cdf7558
Christina Best <cdbest10@gmail.com>
2018-03-30 00:54:24 -0400

    Added EditText fields for user input.

--- app/src/main/java/com/example/android/firebaseexample3_26/MainActivity.java
+++ app/src/main/java/com/example/android/firebaseexample3_26/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.firebaseexample3_26;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,19 +14,31 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference personRef = database.getReference("person");
     private DatabaseReference peopleRef = database.getReference("Multiple people");
-
+    EditText name;
+    EditText ageBox;
+    CheckBox hasGraduated;
+    String nameText = name.getEditableText().toString();
+    String ageText = ageBox.getEditableText().toString();
+    int age = Integer.parseInt(ageText);
+    boolean graduated = hasGraduated.isChecked();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText name = findViewById(R.id.name_field);
+        EditText age = findViewById(R.id.age_field);
+        CheckBox hasGraduated = findViewById(R.id.graduated_field);
+
     }
+
 
     public void set(View view) {
-        personRef.setValue(new Person("MJ", 68, true));
+        personRef.setValue(new Person(nameText, age, graduated));
     }
 
-    public void add(View view){
-        peopleRef.push().setValue(new Person("Lebron", 34, true));
+    public void add(View view) {
+        peopleRef.push().setValue(new Person(nameText, age, graduated));
     }
 }
